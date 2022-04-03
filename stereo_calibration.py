5c9d3ac7bffeadae29989f1062d25a8806d1739c
Shalom Crown <shalomc@airobotics.co.il>
2022-04-03 11:25:24 +0300

    Add Stereo film

--- stereo_calibration.py
+++ stereo_calibration.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 #######################################################################
 #
-#
+# Calibrate a pair of stereo cameras. It is assumed that they
+# are fixed in place relative to each other, and as parallel as possible.
 #
 #######################################################################
 import glob
@@ -20,8 +21,9 @@ if len(sys.argv) < 2:
 
 os.chdir(sys.argv[1])
 
-# Defining the dimensions of checkerboard (minus one in each direction)
-CHECKERBOARD = (6, 9)
+# Defining the dimensions of checkerboard (minus one in each direction, h, w)
+# CHECKERBOARD = (6, 9)
+CHECKERBOARD = (7, 7)
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # Creating vector to store vectors of 3D points for each checkerboard image
@@ -40,10 +42,10 @@ if len(sys.argv) > 3:
     capLeft = cv2.VideoCapture(sys.argv[2])
     capRight = cv2.VideoCapture(sys.argv[3])
 
-    # capLeft.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-    # capLeft.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-    # capRight.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-    # capRight.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+    capLeft.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    capLeft.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+    capRight.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    capRight.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
     imageNumber = 0
     while capLeft.isOpened() and capRight.isOpened():
@@ -58,9 +60,9 @@ if len(sys.argv) > 3:
         grayRight = cv2.cvtColor(rightImage, cv2.COLOR_BGR2GRAY)
 
         leftRet, cornersLeft = cv2.findChessboardCorners(grayLeft, CHECKERBOARD,
-                                                 cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE)
+                            cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE)
         rightRet, cornersRight = cv2.findChessboardCorners(grayRight, CHECKERBOARD,
-                                                 cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE)
+                            cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE)
 
         if not leftRet or not rightRet:
             cv2.imshow("Left Input", leftImage)
@@ -70,13 +72,9 @@ if len(sys.argv) > 3:
                 break
             continue
 
-        objpoints.append(objp)
         # refining pixel coordinates for given 2d points.
         cornersLeft2 = cv2.cornerSubPix(grayLeft, cornersLeft, (11, 11), (-1, -1), criteria)
-        imgpointsleft.append(cornersLeft2)
-
         cornersRight2 = cv2.cornerSubPix(grayRight, cornersRight, (11, 11), (-1, -1), criteria)
-        imgpointsRight.append(cornersRight2)
 
         # Draw and display the corners
         displayLeft = cv2.drawChessboardCorners(leftImage.copy(), CHECKERBOARD, cornersLeft2, leftRet)
@@ -86,6 +84,9 @@ if len(sys.argv) > 3:
         cv2.imshow("Right Input", displayRight)
         key = cv2.waitKey(100)
         if key == 13 or key == 10 or key == 141:
+            objpoints.append(objp)
+            imgpointsleft.append(cornersLeft2)
+            imgpointsRight.append(cornersRight2)
             cv2.imwrite(f"ImageLeft-{imageNumber}.jpg", leftImage)
             cv2.imwrite(f"ImageRight-{imageNumber}.jpg", rightImage)
             print(f"Captured images {imageNumber}")
@@ -127,6 +128,7 @@ else:
 
         cornersRight2 = cv2.cornerSubPix(grayRight, cornersRight, (11, 11), (-1, -1), criteria)
         imgpointsRight.append(cornersRight2)
+        print(f"Read image number {imageNumber}")
 
 cv2.destroyAllWindows()
 
@@ -134,7 +136,7 @@ if not objpoints:
     print("Nothing useful found\n")
     sys.exit(5)
 
-
+print("Calculate camera matrices")
 # Calculate initial calibration matrices
 retLeft, cameraMatrixLeft, distCoeffsleft, rvecsLeft, tvecsLeft = cv2.calibrateCamera(
         objpoints, imgpointsleft, grayLeft.shape[::-1], None, None)
@@ -142,7 +144,11 @@ retLeft, cameraMatrixLeft, distCoeffsleft, rvecsLeft, tvecsLeft = cv2.calibrateC
 retRight, cameraMatrixRight, distCoeffsRight, rvecsRight, tvecsRight = cv2.calibrateCamera(
         objpoints, imgpointsRight, grayRight.shape[::-1], None, None)
 
+print(f"Left camera matrix ret:{retLeft}\n", cameraMatrixLeft)
+print(f"Right camera matrix ret:{retRight}\n", cameraMatrixRight)
+
 # Calculate optimal camera matrices
+print("Calculate optimal camera matrices")
 leftHeight,leftWidth= grayLeft.shape[:2]
 newCameraMatrixLeft, roiLeft = cv2.getOptimalNewCameraMatrix(cameraMatrixLeft, distCoeffsleft,
                                                              (leftWidth,leftHeight), 1, (leftWidth,leftHeight))
@@ -151,6 +157,9 @@ rightHeight, rightWidth= grayRight.shape[:2]
 newCameraMatrixRight, roiRight = cv2.getOptimalNewCameraMatrix(cameraMatrixRight, distCoeffsRight,
                                                                (rightWidth, rightHeight), 1, (rightWidth, rightHeight))
 
+print("Optimal left camera matrix:", newCameraMatrixLeft)
+print("Optimal right camera matrix:", newCameraMatrixRight)
+
 flags = 0
 flags |= cv2.CALIB_FIX_INTRINSIC
 # Here we fix the intrinsic camara matrixes so that only Rot, Trns, Emat and Fmat are calculated.
@@ -158,16 +167,19 @@ flags |= cv2.CALIB_FIX_INTRINSIC
 
 criteria_stereo= (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
+print("Stereo calibration")
 # This step is performed to transformation between the two cameras and calculate Essential and Fundamental matrix
 retS, new_mtxL, distL, new_mtxR, distR, Rot, Trns, Emat, Fmat = cv2.stereoCalibrate(
     objpoints, imgpointsleft, imgpointsRight, newCameraMatrixLeft, distCoeffsleft, newCameraMatrixRight, distCoeffsRight,
     grayLeft.shape[::-1], criteria_stereo, flags)
 
+print("Stereo rectification")
 # Stereo rectification
 rectify_scale = 1
 rect_l, rect_r, proj_mat_l, proj_mat_r, Q, roiL, roiR= cv2.stereoRectify(
     new_mtxL, distL, new_mtxR, distR, grayLeft.shape[::-1], Rot, Trns, rectify_scale,(0,0))
 
+print("Stereo matrices")
 # Calculate mapping matrices
 Left_Stereo_Map = cv2.initUndistortRectifyMap(new_mtxL, distL, rect_l, proj_mat_l,
                                              grayLeft.shape[::-1], cv2.CV_16SC2)
@@ -182,3 +194,4 @@ cv_file.write("Right_Stereo_Map_x", Right_Stereo_Map[0])
 cv_file.write("Right_Stereo_Map_y", Right_Stereo_Map[1])
 cv_file.release()
 cv2.destroyAllWindows()
+print("Done")
---
+++ stereo_film.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python
+#######################################################################
+#
+# Calibrate a pair of stereo cameras. It is assumed that they
+# are fixed in place relative to each other, and as parallel as possible.
+#
+#######################################################################
+import glob
+import os
+
+import cv2
+import numpy as np
+import sys
+
+
+if len(sys.argv) < 3:
+    print("Usage: stereo_film.py  <calibrationFile>  <left input device> <right input device>")
+    print("If no devices are given, existing files will be used")
+    print("Too few command line arguments\n")
+    sys.exit(2)
+
+def nothing(x):
+    pass
+
+def mouseCallback(event, x, y, flags, userdata):
+    disparity = disp[x, y]
+
+
+# Reading the mapping values for stereo image rectification
+cv_file = cv2.FileStorage(sys.argv[1], cv2.FILE_STORAGE_READ)
+Left_Stereo_Map_x = cv_file.getNode("Left_Stereo_Map_x").mat()
+Left_Stereo_Map_y = cv_file.getNode("Left_Stereo_Map_y").mat()
+Right_Stereo_Map_x = cv_file.getNode("Right_Stereo_Map_x").mat()
+Right_Stereo_Map_y = cv_file.getNode("Right_Stereo_Map_y").mat()
+cv_file.release()
+
+# Setting parameters for StereoSGBM algorithm
+minDisparity = 0;
+numDisparities = 64;
+blockSize = 8;
+disp12MaxDiff = 1;
+uniquenessRatio = 10;
+speckleWindowSize = 10;
+speckleRange = 8;
+
+# Creating an object of StereoSGBM algorithm
+stereo = cv2.StereoSGBM_create(minDisparity = minDisparity,
+        numDisparities = numDisparities,
+        blockSize = blockSize,
+        disp12MaxDiff = disp12MaxDiff,
+        uniquenessRatio = uniquenessRatio,
+        speckleWindowSize = speckleWindowSize,
+        speckleRange = speckleRange
+    )
+
+# stereo = cv2.StereoBM_create()
+capLeft = cv2.VideoCapture(sys.argv[2])
+capRight = cv2.VideoCapture(sys.argv[3])
+
+rectifiedDispWindowName = "Rectified Disparity map"
+
+cv2.namedWindow(rectifiedDispWindowName)
+cv2.resizeWindow(rectifiedDispWindowName, 600, 600)
+
+cv2.createTrackbar('numDisparities', rectifiedDispWindowName,1,17,nothing)
+cv2.createTrackbar('blockSize', rectifiedDispWindowName,5,50,nothing)
+cv2.createTrackbar('preFilterType', rectifiedDispWindowName,1,1,nothing)
+cv2.createTrackbar('preFilterSize',rectifiedDispWindowName,2,25,nothing)
+cv2.createTrackbar('preFilterCap',rectifiedDispWindowName,5,62,nothing)
+cv2.createTrackbar('textureThreshold',rectifiedDispWindowName,10,100,nothing)
+cv2.createTrackbar('uniquenessRatio',rectifiedDispWindowName,15,100,nothing)
+cv2.createTrackbar('speckleRange',rectifiedDispWindowName,0,100,nothing)
+cv2.createTrackbar('speckleWindowSize',rectifiedDispWindowName,3,25,nothing)
+cv2.createTrackbar('disp12MaxDiff',rectifiedDispWindowName,5,25,nothing)
+cv2.createTrackbar('minDisparity',rectifiedDispWindowName,5,25,nothing)
+cv2.setMouseCallback(rectifiedDispWindowName, mouseCallback)
+
+
+while capLeft.isOpened() and capRight.isOpened():
+    _, leftImage = capLeft.read()
+    _, rightImage = capRight.read()
+
+    if leftImage is None or rightImage is None:
+        print("Could not read one of the pictures\n")
+        sys.exit(2)
+
+    cv2.imshow("Left image before rectification", leftImage)
+    cv2.imshow("Right image before rectification", rightImage)
+
+    leftRectified = cv2.remap(leftImage, Left_Stereo_Map_x, Left_Stereo_Map_y, cv2.INTER_LANCZOS4, cv2.BORDER_CONSTANT, 0)
+    rightRectified = cv2.remap(rightImage, Right_Stereo_Map_x, Right_Stereo_Map_y, cv2.INTER_LANCZOS4, cv2.BORDER_CONSTANT, 0)
+
+    cv2.imshow("Left image after rectification", leftRectified)
+    cv2.imshow("Right image after rectification", rightRectified)
+
+    # Updating the parameters based on the trackbar positions
+    numDisparities = cv2.getTrackbarPos('numDisparities', rectifiedDispWindowName) * 16
+    blockSize = cv2.getTrackbarPos('blockSize', rectifiedDispWindowName) * 2 + 5
+    preFilterType = cv2.getTrackbarPos('preFilterType', rectifiedDispWindowName)
+    preFilterSize = cv2.getTrackbarPos('preFilterSize', rectifiedDispWindowName) * 2 + 5
+    preFilterCap = cv2.getTrackbarPos('preFilterCap', rectifiedDispWindowName)
+    textureThreshold = cv2.getTrackbarPos('textureThreshold', rectifiedDispWindowName)
+    uniquenessRatio = cv2.getTrackbarPos('uniquenessRatio', rectifiedDispWindowName)
+    speckleRange = cv2.getTrackbarPos('speckleRange', rectifiedDispWindowName)
+    speckleWindowSize = cv2.getTrackbarPos('speckleWindowSize', rectifiedDispWindowName) * 2
+    disp12MaxDiff = cv2.getTrackbarPos('disp12MaxDiff', rectifiedDispWindowName)
+    minDisparity = cv2.getTrackbarPos('minDisparity', rectifiedDispWindowName)
+
+    # Setting the updated parameters before computing disparity map
+    stereo.setNumDisparities(numDisparities)
+    stereo.setBlockSize(blockSize)
+    # stereo.setPreFilterType(preFilterType)
+    # stereo.setPreFilterSize(preFilterSize)
+    stereo.setPreFilterCap(preFilterCap)
+    # stereo.setTextureThreshold(textureThreshold)
+    stereo.setUniquenessRatio(uniquenessRatio)
+    stereo.setSpeckleRange(speckleRange)
+    stereo.setSpeckleWindowSize(speckleWindowSize)
+    stereo.setDisp12MaxDiff(disp12MaxDiff)
+    stereo.setMinDisparity(minDisparity)
+
+    # out = rightRectified.copy()
+    # out[:,:,0] = rightRectified[:,:,0]
+    # out[:,:,1] = rightRectified[:,:,1]
+    # out[:,:,2] = leftRectified[:,:,2]
+
+    out = leftRectified.copy()
+    out[:,:,0] = leftRectified[:,:,0]
+    out[:,:,1] = leftRectified[:,:,1]
+    out[:,:,2] = rightRectified[:,:,2]
+
+    cv2.imshow("Rectified Output image", out)
+
+    grayLeftRectified = cv2.cvtColor(leftRectified, cv2.COLOR_BGR2GRAY)
+    grayRightRectified = cv2.cvtColor(rightRectified, cv2.COLOR_BGR2GRAY)
+
+    disp = stereo.compute(grayLeftRectified, grayRightRectified).astype(np.float32)
+    dispNormalilzed = cv2.normalize(disp, 0, 255, cv2.NORM_MINMAX)
+    cv2.imshow(rectifiedDispWindowName, dispNormalilzed)
+
+    out = leftImage.copy()
+    out[:,:,0] = leftImage[:,:,0]
+    out[:,:,1] = leftImage[:,:,1]
+    out[:,:,2] = rightImage[:,:,2]
+    cv2.imshow("Unrectified Output image", out)
+
+    grayLeft = cv2.cvtColor(leftImage, cv2.COLOR_BGR2GRAY)
+    grayRight = cv2.cvtColor(rightImage, cv2.COLOR_BGR2GRAY)
+
+    dispUnrectified = stereo.compute(grayLeft, grayRight).astype(np.float32)
+    dispUnrectified = cv2.normalize(disp, 0, 255, cv2.NORM_MINMAX)
+    cv2.imshow("Unrectified Disparity map", dispUnrectified)
+
+    key = cv2.waitKey(20)
+
+    if key == 27:
+        break
+
+capLeft.release()
+capRight.release()
+cv2.destroyAllWindows()
